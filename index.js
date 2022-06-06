017c4fc96093a59ebbee9468a800965523241320
chrstno040420 <104917222+chrstno040420@users.noreply.github.com>
2022-06-06 11:23:11 -0500

    commit tree 01fcf4c1dea7c6cdc70cefff6e9e11cf747eff80

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+
+const currentUser = 'Christian Obregon';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+currentUser.slice(0,1);
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
